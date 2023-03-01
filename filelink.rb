297450fbe6457dad318d5c858cd653b5016bc7bb
GauravPandit-Webonise <gaurav.pandit@weboniselab.com>
2023-03-01 21:54:13 +0530

    feature-oop-assignment | oop updated assignment 1

---
+++ filelink.rb
@@ -0,0 +1,15 @@
+module FileLink
+
+  def file_link.write(name, desg, dept)
+    file_name = "employeedetails.csv"
+    CSV.open(file_name, "a") do |file|
+      file << [name. desg, dept]
+    end
+  end
+
+  def file 
+    @@file_name = "employeedetails.csv"
+    file = csv.parse(File.open(@@file_name, 'r'))
+    file
+  end
+end
---
+++ module.rb
@@ -0,0 +1,28 @@
+
+require 'csv'
+
+module Csv
+  def self.write_file(emp_name, emp_contact, emp_mail, emp_gender, emp_age, emp_dob, emp_dept)
+    CSV.open('employeedetails.csv', 'a') do |i|
+      i << [emp_name, emp_contact, emp_mail, emp_gender, emp_age, emp_dob, emp_dept]
+    end
+  end
+
+  def self.read_file
+    puts CSV.read('employeedetails.csv')
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
